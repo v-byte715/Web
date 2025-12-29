3e05b6ca79395f654a33fce38faf0300f08d71eb
v-byte715 <vasanthvigneshwaranm@gmail.com>
2025-12-29 08:07:51 +0530

    Remove assetPrefix from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Web",
-  assetPrefix: "/Web/",
   images: {
     unoptimized: true,
   },
 };
+
 export default nextConfig;
