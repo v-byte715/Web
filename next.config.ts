758c384afb60790ceb09b61a918a41563f8586d5
v-byte715 <vasanthvigneshwaranm@gmail.com>
2025-12-29 07:10:40 +0530

    Update next.config.ts for basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
-  images: {
-    unoptimized: true,
-  },
+  basePath: "/Web",
+  assetPrefix: "/Web/",  
 };
-
 export default nextConfig;
